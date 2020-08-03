958b7971a1018ccdfdfc800a21d7e933132433d4
Syamil <syamil@codelabs.co.id>
2020-08-03 15:23:56 +0700

    add core controller

---
+++ app/controllers/About.php
@@ -0,0 +1,23 @@
+<?php 
+
+/**
+ * 
+ */
+class About 
+{
+	
+	function __construct()
+	{
+		# code...
+	}
+
+	public function index($nama = "Doni(default)")
+	{
+		echo "nama saya $nama";
+	}
+
+	public function page()
+	{
+		echo "about/page";
+	}
+}
