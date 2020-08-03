6313e22ed19ac3e8445587fa32c6aaae90f32b67
Syamil <syamil@codelabs.co.id>
2020-08-03 14:52:45 +0700

    add .htaccess & add url parser

--- app/core/App.php
+++ app/core/App.php
@@ -8,6 +8,19 @@ class App
 	
 	function __construct()
 	{
-		# code...
+		$url = $this->parseUrl();
+
+		var_dump($url);
+	}
+
+	public function parseUrl()
+	{
+		if ( isset($_GET['url']) ) {
+			$url = rtrim($_GET['url'], '/');
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			$url = explode('/', $url);
+			
+			return $url;
+		}
 	}
 }
